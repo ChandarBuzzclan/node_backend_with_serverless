de484c6bb616a57cd867ccd1536ab6fb08a467bf
ramesh-buzzclan <ramesh.singh@buzzclan.com>
2024-02-21 19:25:20 +0530

    downgrade middy versions to fix the issues

--- src/function/handler.ts
+++ src/function/handler.ts
@@ -1,16 +1,18 @@
-  import middy from '@middy/core'
-  import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda'
-  
-  export const hello = middy<APIGatewayProxyEvent, APIGatewayProxyResult>()
-    .handler(async (req, context) => {
-      // The context type gets augmented here by the secretsManager middleware.
-      // This is just an example, obviously don't ever log your secret in real life!
-    
-      return {
-        statusCode: 200,
-        body: JSON.stringify({
-          message: `Hello from `,
-          req
-        })
-      }
-    })  
+import middy from "@middy/core";
+import { APIGatewayProxyEvent, APIGatewayProxyResult } from "aws-lambda";
+
+export const hello = middy<
+  APIGatewayProxyEvent,
+  APIGatewayProxyResult
+>().handler(async (req: any, context: any) => {
+  // The context type gets augmented here by the secretsManager middleware.
+  // This is just an example, obviously don't ever log your secret in real life!
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+      message: `Hello from `,
+      req,
+    }),
+  };
+});
