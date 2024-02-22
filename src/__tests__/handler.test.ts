b25faa8b216615f7111b82f382e9b11798818f55
ramesh-buzzclan <ramesh.singh@buzzclan.com>
2024-02-22 15:26:57 +0530

    changed  folder name for unitily and added winston logger

--- src/__tests__/handler.test.ts
+++ src/__tests__/handler.test.ts
@@ -1,8 +1,5 @@
 import { APIGatewayEvent } from 'aws-lambda';
 import helloHandler from '../function/handler'; // Import the file containing your lambda handler
-
-describe('helloHandler', () => {
-  it("should return 'Hello World' message when no query parameter is provided", async () => {
     const event: APIGatewayEvent = { queryStringParameters: null } as APIGatewayEvent;
     const result = await helloHandler(event);
 
